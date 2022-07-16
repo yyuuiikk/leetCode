66c3eb2b7c7b8199430a97749a97aa09517ca446
Yuta Ikawa <yutaikawa0315@gmail.com>
2022-07-16 20:21:53 +0900

    1480 (#2)
    * add 1480
    
    * mod
    
    * mod

---
+++ 1480_Running_Sum_of_1d_Array.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function runningSum($nums) {
+        for ($i = 1; $i < count($nums); $i++) {
+            $nums[$i] += $nums[$i-1];
+        }
+        return $nums;
+    }
+}
