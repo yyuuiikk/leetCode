6e8db2f1217a104fbb3a4b76e4bdea3c76e916b2
yyuuiikk <yutaikawa0315@gmail.com>
2022-07-16 22:01:45 +0900

    add answer

---
+++ 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.php
@@ -0,0 +1,21 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return Integer
+     */
+    function numberOfSteps($num) {
+        $count = 0;
+        while ($num > 1) {
+            if ($num % 2 === 0) {
+                $num = intdiv($num, 2);
+            } else {
+                $num = $num - 1;
+            }
+            $count++;
+        }
+        return ++$count;
+    }
+}
